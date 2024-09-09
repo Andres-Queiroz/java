d07f3ee82910dd218a459a07358a61a191d1bc2c
Andres Queiroz <andres.franca@oceane.com.br>
2024-09-09 10:10:26 -0300

    Att segundo dia

--- segundodiaa/src/aula/andres/segundodia/segundodia.java
+++ segundodiaa/src/aula/andres/segundodia/segundodia.java
@@ -4,5 +4,8 @@ public class segundodia {
         int n = 7;
         for (int i = 0; i<=10; i++)
         System.out.println(n + " x " + i +" = " +n*i);
+
+        String resultado = (n*n) == 49 ? "O número é 7" : "O número não é 7";
+        System.out.println(resultado);
     }
 }
