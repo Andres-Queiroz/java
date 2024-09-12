d1b26a9084099153dd0b0857c0870c67a2569f02
Andres Queiroz <andres.franca@oceane.com.br>
2024-09-12 15:17:52 -0300

    ultimo commit

---
+++ aulas/src/aula/andres/quintodia/quintodiaforearray.java
@@ -0,0 +1,12 @@
+package aula.andres.quintodia;
+
+public class quintodiaforearray {
+    public static void main (){
+        String alunos [] = {"Andres", "Yuri", "Airton", "Leandro", "Lucas", "Igor", "Douglas", "João"};
+
+
+        for(int i = 0; i<alunos.length;i++){
+            System.out.println(alunos[i]);
+        }
+    }
+}
